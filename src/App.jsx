93596d2f88328a2bf196403f4f6454fe06072eab
dani31perez <perezbarredad@gmail.com>
2025-03-15 22:28:35 -0600

    Implemented conditional rendering based on authentication

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,25 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import DashBoard from "./components/DashBoard";
+import { useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebaseConfig";
+
 
 function App() {
+  const [user, setUser] = useState("");
+
+  useEffect(() => {
+    const subscribed = onAuthStateChanged(auth, (currUser) => setUser(currUser));
+    return () => subscribed();
+  }, []);
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />}/>
-        <Route path="/register" element={<Register />}/>
-        <Route path="/dashboard" element={<DashBoard />}/>
+        <Route path="/" element={user ? <DashBoard /> :<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={user ? <DashBoard /> :<Navigate to="/"/>} />
       </Routes>
     </BrowserRouter>
   );
