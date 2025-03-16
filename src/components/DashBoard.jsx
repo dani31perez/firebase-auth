d7fa99808af2b69b75eae03ba53ccdeaa4954325
dani31perez <perezbarredad@gmail.com>
2025-03-15 23:23:18 -0600

    Implemented logout functionality with Firebase

--- src/components/DashBoard.jsx
+++ src/components/DashBoard.jsx
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Container } from "react-bootstrap";
+import { useNavigate } from "react-router";
+import { auth } from "../firebaseConfig";
+import { signOut } from "firebase/auth";
 
 function DashBoard() {
+  const navigate = useNavigate();
+  const user = auth.currentUser;
+  const [error, setError] = useState("");
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      navigate("/");
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
   return (
     <Container className="text-center my-5">
-      <p  style={{fontSize: "70px"}}> DashBoard</p>
+      <p style={{ fontSize: "70px" }}> DashBoard</p>
       <p className="fs-1 mb-5"> Welcome</p>
-      <Button variant="outline-danger" size="lg">
+      <Button variant="outline-danger" size="lg" onClick={handleLogout}>
         Logout
       </Button>
+      {error && (
+        <Alert className="mt-5" key="alert" variant="danger">
+          {error}
+        </Alert>
+      )}
     </Container>
   );
 }
